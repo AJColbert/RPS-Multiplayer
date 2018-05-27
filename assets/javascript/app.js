62334c45eb757a4bef4ed5adc4d1566bacc9668d
Aaron Colbert <aaron-colbert@hotmail.com>
2018-05-27 11:22:32 -0400

    Completed Name and score Display for all senerios of players and connections

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 
 var PlayerNumber = 0;
 var PlayerKey;
+var Player;
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyAL5x0RCXzShzYrsG3pnzunppU-aFYEZSA",
@@ -15,109 +16,226 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 // var playerconnRef = database.ref("/Players");
-
+//Button Click Add User and Print to Plater Profile
 $(".btn-start").on("click", function (event)
 {
     event.preventDefault();
 
     var name = $("#input-name").val().trim();
 
-    var Player = {
+    Player = {
         name: name,
         choice: "",
         loses: 0,
         wins: 0
     };
-
     $(".game-info").empty();
     var h3 = $("<h3>");
-    h3.text("Hi " + name + "! You are Player#");
+    h3.text("Hi " + name + "! You are Player " + PlayerNumber);
     $(".game-info").append(h3);
 
-    console.log(Player.name);
-    console.log(Player.choice);
-    console.log(Player.loses);
-    console.log(Player.wins);
+    database.ref("/Players").once("value", function (childSnapshot)
+    {
+        var JName;
+        var Jscore;
+        var numchild = childSnapshot.numChildren();
+        var name = Player.name;
+        var wins = Player.wins;
+        var loses = Player.loses;
 
-    database.ref("/Players").push(Player);
+        if (numchild == 0)
+        {
+            PlayerNumber = 1;
 
-    alert("Successfully Added a New Employee");
+            database.ref("/Players").child(PlayerNumber).set(Player);
 
-    $("#employeename").text("");
-    $("#role").text("");
-    $("#startdate").text("");
-    $("#monthlyrate").text("");
+            DisplayPlayerInfo(PlayerNumber, Player);
 
+            database.ref("/Players").child(PlayerNumber).onDisconnect().remove();
+        }
+        else if (numchild == 1)
+        {
+            childKey = parseInt(Object.keys(childSnapshot.val())[0]);
+
+            if (childKey == 1)
+            {
+                PlayerNumber = 2;
+            }
+            else if (childKey == 2)
+            {
+                PlayerNumber = 1;
+            }
+
+            database.ref("/Players").child(PlayerNumber).set(Player);
+
+            DisplayPlayerInfo(PlayerNumber, Player);
+
+            database.ref("/Players").child(PlayerNumber).onDisconnect().remove();
+        }
+        else
+        {
+            PlayerNumber = 3;
+            $(".game-info").empty();
+            var h3 = $("<h3>");
+            h3.text("To Late! There's a Game in Progress. Try Again Later");
+            $(".game-info").append(h3);
+        }
+    }, function (errorObject)
+        {
+            console.log("The read failed: " + errorObject.code);
+        });
 });
 
-
-database.ref("/Players").on("child_added", function (childSnapshot)
+//Firts Time Connecting Player Data Population
+database.ref("/Players").once("value", function (childSnapshot)
 {
-    var JName;
-    var Jscore;
-    console.log("childadd")
-    console.log(childSnapshot.key);
+    var numchild = childSnapshot.numChildren();
 
-    PlayerNumber += 1;
-console.log(PlayerNumber)
-    var name = childSnapshot.val().name;
-    var wins = childSnapshot.val().wins;
-    var loses = childSnapshot.val().loses;
-
-    switch (PlayerNumber)
+    if (numchild == 0)
     {
-        case 1:
-        PlayerKey = childSnapshot.key
-            JName = $(".player1-Name");
-            Jscore = $(".player1-score");
-            break;
+    }
+    else if (numchild == 1)
+    {
+        var CurrentKey = Object.keys(childSnapshot.val())[0];
+        if (CurrentKey == 1)
+        {
+            PlayerNumber = 2;
+        }
+        else if (CurrentKey == 2)
+        {
+            PlayerNumber = 1;
+        }
 
-        case 2:
-        PlayerKey = childSnapshot.key
-            JName = $(".player2-Name");
-            Jscore = $(".player2-score");
-            break;
+        DisplayPlayerInfo(CurrentKey, childSnapshot.child(CurrentKey).val());
 
-        default:
-    };
-    console.log("KEY" + PlayerKey);
-    JName.empty();
-    Jscore.empty();
-    
-    var h4Name = $("<h4>");
-    h4Name.text(name);
-    JName.append(h4Name);
+    }
+    else if (numchild == 2)
+    {
+        $(".game-info").empty();
+        var h3 = $("<h3>");
+        h3.text("To Late! There's a Game in Progress. Try Again Later");
+        $(".game-info").append(h3);
 
-    var h4winlose = $("<h4>");
-    h4winlose.text("Wins: " + wins + "  Loses: " + loses)
-    Jscore.append(h4winlose);
+        DisplayPlayerInfo(1, childSnapshot.child("1").val());
 
+        DisplayPlayerInfo(2, childSnapshot.child("2").val());
+    }
 }, function (errorObject)
     {
         console.log("The read failed: " + errorObject.code);
     });
 
 
-
-
-function PlayerInfo()
+var DisplayPlayerInfo = function (PlayerNumber, SnapChild)
 {
-    //Determine which player they are use switch to execute
+    if (SnapChild != null)
+    {
+        var JName;
+        var Jscore;
 
+        if (PlayerNumber == 1)
+        {
+            //Display Player1
+            JName = $(".player1-Name");
+            Jscore = $(".player1-score");
+        }
+        else if (PlayerNumber == 2)
+        {
+            //Display Player1
+            JName = $(".player2-Name");
+            Jscore = $(".player2-score");
+        }
 
+        var name = SnapChild.name;
+        var wins = SnapChild.wins;
+        var loses = SnapChild.loses;
+        JName.empty();
+        Jscore.empty();
 
+        var h4Name = $("<h4>");
+        h4Name.text(name);
+        JName.append(h4Name);
 
-    var h2Name = $("<h2>");
-    h2Name.text(name);
-
-    var h2winlose = $("<h2>");
-    h2winlose.text("Wins: " + wins + "Loses: " + loses)
-
-
-
-
-
+        var h4winlose = $("<h4>");
+        h4winlose.text("Wins: " + wins + "  Loses: " + loses)
+        Jscore.append(h4winlose);
+    }
 }
+
+//For Every Value Update to Database 
+database.ref("/Players").on("value", function (Snapshot)
+{
+    console.log("ON Value")
+    var numchild = Snapshot.numChildren();
+    console.log(numchild);
+    if (numchild == 0)
+    {
+        JName = $(".player2-Name");
+        Jscore = $(".player2-score");
+        JName.empty();
+        Jscore.empty();
+        var h3Name = $("<h3>");
+        h3Name.text("Waiting For Player 2");
+        JName.append(h3Name);
+
+        JName = $(".player1-Name");
+        Jscore = $(".player1-score");
+        JName.empty();
+        Jscore.empty();
+        var h3Name = $("<h3>");
+        h3Name.text("Waiting For Player 1");
+        JName.append(h3Name);
+    }
+    else if (numchild == 1)
+    {
+        var JName;
+        var Jscore;
+        CurrentKey = Object.keys(Snapshot.val())[0];
+        if (CurrentKey == 1)
+        {
+            JName = $(".player2-Name");
+            Jscore = $(".player2-score");
+            JName.empty();
+            Jscore.empty();
+            var h3Name = $("<h3>");
+            h3Name.text("Waiting For Player 2");
+            JName.append(h3Name);
+
+            DisplayPlayerInfo(1, Snapshot.child("1").val());
+
+        }
+        else if (CurrentKey == 2)
+        {
+            JName = $(".player1-Name");
+            Jscore = $(".player1-score");
+            JName.empty();
+            Jscore.empty();
+            var h3Name = $("<h3>");
+            h3Name.text("Waiting For Player 1");
+            JName.append(h3Name);
+
+            DisplayPlayerInfo(2, Snapshot.child("2").val());
+        }
+
+    }
+    else if (numchild == 2)
+    {
+        if (PlayerNumber != 1 && PlayerNumber != 2)
+        {
+            $(".game-info").empty();
+            var h3 = $("<h3>");
+            h3.text("To Late! There's a Game in Progress. Try Again Later");
+            $(".game-info").append(h3);
+        }
+        DisplayPlayerInfo(1, Snapshot.child("1").val());
+
+        DisplayPlayerInfo(2, Snapshot.child("2").val());
+    }
+
+}, function (errorObject)
+    {
+        console.log("The read failed: " + errorObject.code);
+    });
 
 
 
@@ -128,7 +246,8 @@ function PlayerInfo()
 // connectionsRef references a specific location in our database.
 // All of our connections will be stored in this directory.
 var connectionsRef = database.ref("/connections");
-var playerref = database.ref();
+var playerref = database.ref("/Players");
+//console.log("check: " + playerref.child(1) + "Connec: " + connectionsRef)
 // '.info/connected' is a special location provided by Firebase that is updated every time
 // the client's connection state changes.
 // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
@@ -147,7 +266,7 @@ connectedRef.on("value", function (snap)
 
         // Remove user from the connection list when they disconnect.
         con.onDisconnect().remove();
-        playerref.child(PlayerKey).remove();
+        // database.ref("/Players").child(PlayerKey).onDisconnect().remove();
     }
 });
 
@@ -158,6 +277,8 @@ connectionsRef.on("value", function (snap)
     // Display the viewer count in the html.
     // The number of online users is the number of children in the connections list.
     $("#connected-viewers").text(snap.numChildren());
+
+    PlayerKey = snap.key;
 });
 
 // -------------------------------------------------------------- (CRITICAL - BLOCK) --------------------------- //
