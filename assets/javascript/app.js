4e3e043189c8826dc8837867abb784e5789aba97
Aaron Colbert <aaron-colbert@hotmail.com>
2018-05-28 19:17:04 -0400

    Completed Game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@
 var PlayerNumber = 0;
 var PlayerKey;
 var Player;
+var RoundOver = false;
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyAL5x0RCXzShzYrsG3pnzunppU-aFYEZSA",
@@ -15,7 +16,6 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-// var playerconnRef = database.ref("/Players");
 //Button Click Add User and Print to Plater Profile
 $(".btn-start").on("click", function (event)
 {
@@ -29,10 +29,6 @@ $(".btn-start").on("click", function (event)
         loses: 0,
         wins: 0
     };
-    $(".game-info").empty();
-    var h3 = $("<h3>");
-    h3.text("Hi " + name + "! You are Player " + PlayerNumber);
-    $(".game-info").append(h3);
 
     database.ref("/Players").once("value", function (childSnapshot)
     {
@@ -46,6 +42,11 @@ $(".btn-start").on("click", function (event)
         if (numchild == 0)
         {
             PlayerNumber = 1;
+
+            $(".game-info").empty();
+            var h3 = $("<h3>");
+            h3.text("Hi " + name + "! You are Player " + PlayerNumber);
+            $(".game-info").append(h3);
 
             database.ref("/Players").child(PlayerNumber).set(Player);
 
@@ -67,7 +68,10 @@ $(".btn-start").on("click", function (event)
             }
 
             database.ref("/Players").child(PlayerNumber).set(Player);
-
+            $(".game-info").empty();
+            var h3 = $("<h3>");
+            h3.text("Hi " + name + "! You are Player " + PlayerNumber);
+            $(".game-info").append(h3);
             DisplayPlayerInfo(PlayerNumber, Player);
 
             database.ref("/Players").child(PlayerNumber).onDisconnect().remove();
@@ -152,7 +156,7 @@ var DisplayPlayerInfo = function (PlayerNumber, SnapChild)
         JName.empty();
         Jscore.empty();
 
-        var h4Name = $("<h4>");
+        var h4Name = $("<h3>");
         h4Name.text(name);
         JName.append(h4Name);
 
@@ -220,6 +224,11 @@ database.ref("/Players").on("value", function (Snapshot)
     }
     else if (numchild == 2)
     {
+
+        DisplayPlayerInfo(1, Snapshot.child("1").val());
+        DisplayPlayerInfo(2, Snapshot.child("2").val());
+
+        console.log("Ready to start Playing");
         if (PlayerNumber != 1 && PlayerNumber != 2)
         {
             $(".game-info").empty();
@@ -227,19 +236,304 @@ database.ref("/Players").on("value", function (Snapshot)
             h3.text("To Late! There's a Game in Progress. Try Again Later");
             $(".game-info").append(h3);
         }
-        DisplayPlayerInfo(1, Snapshot.child("1").val());
+        console.log(RoundOver);
+        if (RoundOver)
+        {
+            setTimeout(function ()
+            {
+                $(".wait2").remove();
+                $(".player1-r").empty();
+                $(".player1-p").empty();
+                $(".player1-s").empty();
+                $(".player2-r").empty();
+                $(".player2-p").empty();
+                $(".player2-s").empty();
+                $(".result").empty();
+                RoundOver = false;
+                PlayGame(Snapshot);
+            }, 5000)
 
-        DisplayPlayerInfo(2, Snapshot.child("2").val());
+            
+        } else
+        {
+            PlayGame(Snapshot);
+        }
     }
 
 }, function (errorObject)
     {
         console.log("The read failed: " + errorObject.code);
     });
+//Execute game function
+var PlayGame = function (Snapshot)
+{
+    var Player1Choice = Snapshot.child("1").child("choice").val()
+    var Player2Choice = Snapshot.child("2").child("choice").val()
+    console.log(Player1Choice);
+    console.log(Player2Choice);
+    
+
+    if (Player1Choice == "")
+    {
+        if (PlayerNumber == 1)
+        {
+            if ($(".yourturn").length)
+            {
+                $(".yourturn").remove();
+            }
+            //NOT Drawing for some reason
+            var h3 = $("<h3>");
+            h3.text("It's Your Turn!");
+            h3.attr("class", "yourturn");
+            $(".game-info").append(h3);
+            console.log(h3)
+            addRPS(1);
+        }
+        else
+        {
+            if ($(".wait1").length)
+            {
+                $(".wait1").remove();
+            }
+
+            var h3turn = $("<h3>");
+            h3turn.attr("class", "wait1");
+            h3turn.html("Waiting For Player 1");
+            $(".game-info").append(h3turn);
+        }
+    }
+    else if (Player1Choice != "" && Player2Choice == "")
+    {
+        if (PlayerNumber == 2)
+        {
+            if ($(".yourturn").length)
+            {
+                $(".yourturn").remove();
+            }
+
+            $("wait1").remove();
+            //NOT Drawing for some reason
+            var h3 = $("<h3>");
+            h3.attr("class", "yourturn");
+            h3.text("It's Your Turn!");
+            $(".game-info").append(h3);
+            console.log(h3)
+            addRPS(2);
+        }
+        else
+        {  
+            var h3turn = $("<h3>");
+            h3turn.attr("class", "wait2");
+            h3turn.html("Waiting For Player 2");
+            $(".game-info").append(h3turn);
+        }
+
+    }
+    else if (Player1Choice != "" && Player2Choice != "")
+    {
+       
+        $(".wait2").remove();
+    
+        Player1Obj = Snapshot.child("1").val();
+        Player2Obj = Snapshot.child("2").val()
+
+        // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+        if ((Player1Choice === "Rock") || (Player1Choice === "Paper") || (Player1Choice === "Scissors"))
+        {
+            if ((Player1Choice === "Rock") && (Player2Choice === "Scissors"))
+            {
+                Player1Win(Player1Obj, Player2Obj);
+            } else if ((Player1Choice === "Rock") && (Player2Choice === "Paper"))
+            {
+                Player2Win(Player1Obj, Player2Obj);
+            } else if ((Player1Choice === "Scissors") && (Player2Choice === "Rock"))
+            {
+                Player2Win(Player1Obj, Player2Obj);
+            } else if ((Player1Choice === "Scissors") && (Player2Choice === "Paper"))
+            {
+                Player1Win(Player1Obj, Player2Obj);
+            } else if ((Player1Choice === "Paper") && (Player2Choice === "Rock"))
+            {
+                Player1Win(Player1Obj, Player2Obj);
+            } else if ((Player1Choice === "Paper") && (Player2Choice === "Scissors"))
+            {
+                Player2Win(Player1Obj, Player2Obj);
+            } else if (Player1Choice === Player2Choice)
+            {
+                TieGame(Player1Obj, Player2Obj);
+            }
+        }
+
+        //game logic make new func maybe
+        /*  game logic from old rps game
+        Clear controls
+        declare winner
+        update winlose via set functions
+        clear winner field */
+
+    }
+}
+
+//Get Value of choice that was clicked
+$(document).on("click", ".rps", function ()
+{
+    var Playerchoice = $(this).text();
+
+    if (PlayerNumber == 1)
+    {
+        database.ref("/Players").child("1").update({
+            choice: Playerchoice
+        });
+        $(".player1-r").empty();
+        $(".player1-p").empty();
+        $(".player1-s").empty();
+
+        $(".yourturn").remove();
+
+        var h1Choice = $("<h1>");
+        h1Choice.text(Playerchoice);
+        $(".player1-r").append(h1Choice);
+    }
+    else if (PlayerNumber == 2)
+    {
+        database.ref("/Players").child("2").update({
+            choice: Playerchoice
+        });
+        $(".player2-r").empty();
+        $(".player2-p").empty();
+        $(".player2-s").empty();
+
+        var h1Choice = $("<h1>");
+        h1Choice.text(Playerchoice);
+        $(".player2-r").append(h1Choice);
+        $(".yourturn").remove();
+    }
+});
+
+var Player1Win = function (Player1Obj, Player2Obj)
+{
+    RoundOver = true;
+    win = parseInt(Player1Obj.wins) + 1;
+    lose = parseInt(Player2Obj.loses) + 1;
+
+    var h1Result = $("<h1>");
+    h1Result.text(Player1Obj.name + " Wins");
+    $(".result").append(h1Result);
+
+    database.ref("/Players").child("1").update({
+        choice: "",
+        wins: win
+    });
+
+    database.ref("/Players").child("2").update({
+        choice: "",
+        loses: lose
+    });
+
+}
+
+var Player2Win = function (Player1Obj, Player2Obj)
+{
+    RoundOver = true;
+    win = parseInt(Player2Obj.wins) + 1;
+    lose = parseInt(Player1Obj.loses) + 1;
+
+    var h1Result = $("<h1>");
+    h1Result.text(Player2Obj.name + " Wins");
+    $(".result").append(h1Result);
+
+    database.ref("/Players").child("1").update({
+        choice: "",
+        wins: win
+    });
+
+    database.ref("/Players").child("2").update({
+        choice: "",
+        loses: lose
+    });
+
+}
+
+var TieGame = function (Player1Obj, Player2Obj)
+{
+    RoundOver = true;
+    var h1Result = $("<h1>");
+    h1Result.text("Tie Game");
+    $(".result").append(h1Result);
+
+    database.ref("/Players").child("1").update({
+        choice: ""
+    });
+
+    database.ref("/Players").child("2").update({
+        choice: ""
+    });
+}
 
 
 
+database.ref("/Players").child(1).child("choice").on("value", function (Snapshot)
+{
+    console.log("Spefific child value change child1 child choice")
+}, function (errorObject)
+    {
+        console.log("The read failed: " + errorObject.code);
+    });
 
+var addRPS = function (PlayerRef)
+{
+    var Jr;
+    var Jp;
+    var Js;
+    if (PlayerRef == 1)
+    {
+        Jr = $(".player1-r");
+        Jp = $(".player1-p");
+        Js = $(".player1-s");
+
+        Jr.empty();
+        var h3r = $("<h4>");
+        h3r.attr("class", "rps");
+        h3r.text("Rock");
+        Jr.append(h3r);
+
+        Jp.empty();
+        var h3p = $("<h4>");
+        h3p.attr("class", "rps");
+        h3p.text("Paper");
+        Jp.append(h3p);
+
+        Js.empty();
+        var h3s = $("<h4>");
+        h3s.attr("class", "rps");
+        h3s.text("Scissors");
+        Js.append(h3s);
+    }
+    else if (PlayerRef == 2)
+    {
+        Jr = $(".player2-r");
+        Jp = $(".player2-p");
+        Js = $(".player2-s");
+
+        Jr.empty();
+        var h3r = $("<h4>");
+        h3r.attr("class", "rps");
+        h3r.text("Rock");
+        Jr.append(h3r);
+
+        Jp.empty();
+        var h3p = $("<h4>");
+        h3p.attr("class", "rps");
+        h3p.text("Paper");
+        Jp.append(h3p);
+
+        Js.empty();
+        var h3s = $("<h4>");
+        h3s.attr("class", "rps");
+        h3s.text("Scissors");
+        Js.append(h3s);
+    }
+}
 
 
 // -------------------------------------------------------------- (CRITICAL - BLOCK) --------------------------- //
@@ -282,5 +576,4 @@ connectionsRef.on("value", function (snap)
 });
 
 // -------------------------------------------------------------- (CRITICAL - BLOCK) --------------------------- //
-
 
